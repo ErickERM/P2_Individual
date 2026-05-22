e763f833dc4bf38b61b128c276b651d3524cb263
ErickERM <emanuelerick556@gmail.com>
2026-05-21 18:06:51 -0600

    .

---
+++ operacionesBasicas.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+numeros = np.array([1,7,8,3])
+
+resultado = numeros +5
+res = numeros **5
+
+promedio = np.mean(numeros)
+maximo = np.max(numeros)
+minimo = np.min(numeros)
+suma = np.sum(numeros)
+
+print(f"Promedio: {promedio}")
+print(f"Máximo: {maximo}")
+print(f"Mínimo: {minimo}")
+print(f"Suma: {suma}")
+print(resultado)
+print(res)
